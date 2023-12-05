ca2d53e79abb2aaac39772b9113561bad8d79563
Zaw Zaw Myat Nyein <zawzawmyatnyein@gmail.com>
2023-12-05 15:11:49 +0630

    setup project

---
+++ src/setupDatabase.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import { config } from './config';
+import Logger from 'bunyan';
+
+const log: Logger = config.createLogger('setupDatabase');
+
+export default () => {
+  const connect = () => {
+    mongoose
+      .connect(`${config.DATABASE_URL}`)
+      .then(() => {
+        log.info('Succefully connected to database.');
+      })
+      .catch((error) => {
+        log.error('Error connecting to database', error);
+        return process.exit(1);
+      });
+  };
+  connect();
+
+  mongoose.connection.on('disconnected', connect);
+};
+
+// mhFvABG9g9YLUoUI
